4c05c3dc016dcf5db88e463af1ca2c5261898432
Tajbir Sandhu <sandhu_tajbir@outlook.com>
2018-10-30 18:13:49 -0700

    Added if statement incase key is not in range of the array

--- 328Lab1B/main.cpp
+++ 328Lab1B/main.cpp
@@ -10,78 +10,26 @@ using namespace std;
 
 int linearSearch(std::vector<int> a, int key)
 {
-	auto start = chrono::high_resolution_clock::now();
 	for (int i = 0; i < a.size(); i++)
 	{
 		if (a[i] == key)
 			return key;
 	}
-	auto end = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::microseconds>((end - start));
-	cout << "Linear search lasts " << duration.count() << " microseconds" << endl;
 	return -1;
 }
 
-//int binarySearch(std::vector<int> a, int l, int r, int key)
-//{
-//	int split = a.size() / 2;
-//
-//	auto start = chrono::high_resolution_clock::now();
-//	if (key == a[split])
-//	{
-//		return a[split];
-//	}
-//	auto end = chrono::high_resolution_clock::now();
-//	auto duration = chrono::duration_cast<chrono::microseconds>((end - start));
-//	cout << "Part one lasts " << duration.count() << " microseconds" << endl;
-//	start = chrono::high_resolution_clock::now();
-//	if (key < a[split])
-//	{
-//		return binarySearch(a, l, split - 1, key);
-//	}
-//	end = chrono::high_resolution_clock::now();
-//	duration = chrono::duration_cast<chrono::microseconds>((end - start));
-//	cout << "Part two lasts " << duration.count() << " microseconds" << endl;
-//	start = chrono::high_resolution_clock::now();
-//	if (key > a[split])
-//	{
-//		return binarySearch(a, split + 1, r, key);
-//	}
-//	end = chrono::high_resolution_clock::now();
-//	duration = chrono::duration_cast<chrono::microseconds>((end - start));
-//	cout << "Part three lasts " << duration.count() << " microseconds" << endl;
-//	return -1;
-//}
 int binarySearch(std::vector<int> a, int l, int r, int key)
 {
 	int split = (r + l) / 2;
 	int left = l;
 	int right = r;
-	if (key == a[split])
-		return a[split];
-	else
+	if (l > r)
 	{
-		
-		while(a[split] != key)
-		{
-			auto start = chrono::high_resolution_clock::now();
-			if (a[split] < key)
-			{
-				left = split;
-				split = (right + split) / 2;
-			}
-			else if (a[split] > key)
-			{
-				right = split;
-				split = (left + split) / 2;
-			}
-			auto end = chrono::high_resolution_clock::now();
-			auto duration = chrono::duration_cast<chrono::microseconds>((end - start));
-			cout << "Part one lasts " << duration.count() << " microseconds" << endl;
-		}
-		return split;
+		return 0;
 	}
-	/*else if (key < a[split])
+	if (key == a[split])
+		return split;
+	else if (key < a[split])
 	{
 		return binarySearch(a, l, split - 1, key);
 	}
@@ -89,8 +37,7 @@ int binarySearch(std::vector<int> a, int l, int r, int key)
 	{
 		return binarySearch(a, split + 1, r, key);
 	}
-	else return -1;*/
-
+	
 }
 
 int main()
@@ -108,12 +55,20 @@ int main()
 
 
 	sort(vec.begin(), vec.end());
-	cout << endl;
 	int a = 5000;
 
+	auto start = chrono::high_resolution_clock::now();
 	linearSearch(vec, a);
-	cout << binarySearch(vec, 0, vec.size() - 1, a);
+	auto end = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>((end - start));
+	cout << "Linear search lasts " << duration.count() << " microseconds" << endl;
+	
+	start = chrono::high_resolution_clock::now();
+	binarySearch(vec, 0, vec.size() - 1, a);
+	end = chrono::high_resolution_clock::now();
+	duration = chrono::duration_cast<chrono::microseconds>((end - start));
+	cout << "Binary search lasts " << duration.count() << " microseconds" << endl;
 
-
+	return 0;
 
 }
